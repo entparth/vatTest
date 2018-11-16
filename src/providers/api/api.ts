5d187b21bbda24b2a7eea700b6fe6f79da701853
entparth <parth.sharma@eyt.ee>
2018-11-16 14:06:29 +0530

    final submission

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -2,12 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the ApiProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ApiProvider {
   baseurl: string = 'https://vat.erply.com/numbers?vatNumber=';
@@ -21,13 +15,9 @@ export class ApiProvider {
       console.log(vatNumber)
       var url = this.baseurl + vatNumber ;
       console.log(url)
-      
 			// this will call  api url for fetching vat details
 			this.http.get(url).map(res => res).subscribe((data) => {
-
 				console.log("api  data API -> ", data);
-				// Save data to Local Storage
-				// this.storage.set('categories', data);
 				resolve(data);
 			},
 				err => {
